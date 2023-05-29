b946db0f90b9436ea2a1ecf19ad9ffc69d82dab7
Clayton Dewey <clay@dryadmedia.com>
2023-05-29 14:34:09 -0500

    Implement steeringControl

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import { getObstacleEvents } from './computer-vision';
-// https://www.codecademy.com/learn/learn-typescript
 interface AutonomousCar {
   isRunning: boolean;
   respond: (events: Events) => void;
@@ -19,18 +18,33 @@ interface Steering extends Control {
 
 interface AutonomousCarProps {
   isRunning: boolean;
+  steeringControl: Steering;
 }
 
 class Car implements AutonomousCar {
   isRunning;
+  steeringControl;
   constructor(props: AutonomousCarProps) {
     this.isRunning = props.isRunning;
+    this.steeringControl = props.steeringControl;
   }
 
   respond(events: Events) {
     if (!this.isRunning) {
       console.log('The car is off.');
     }
+    let eventKeys = Object.keys(events);
+    eventKeys.forEach((eventKey) => {
+      if (!events[eventKey]) return;
+
+      if (eventKey === 'ObstacleLeft') {
+        this.steeringControl.turn('right');
+      }
+
+      if (eventKey === 'ObstacleRight') {
+        this.steeringControl.turn('left');
+      }
+    });
   }
 }
 
@@ -44,11 +58,17 @@ class SteeringControl implements Steering {
   }
 }
 
+const steering = new SteeringControl();
+
 const autonomousCar = new Car({
   isRunning: true,
+  steeringControl: steering,
 });
 
-const steering = new SteeringControl();
-steering.turn('right');
-
 console.log(autonomousCar.respond(getObstacleEvents()));
+
+/* 
+    Write code that will call .respond() many times with new events to see a sequence of turns.
+    Write code that allows the car to accelerate or decelerate based on the event. You could make this another type that extends the Control type.
+    Add more types of events so your car can do things like ‘emergencyBrake’ or ‘parallelPark’.
+ */
